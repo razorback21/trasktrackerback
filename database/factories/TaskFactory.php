cde67e2ad5ce6c100ce223ab04d3511bf07b2499
donz <donnygapulao@gmail.com>
2025-11-18 22:24:47 +0800

    task factory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    protected $model = Task::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(4),
+            'description' => $this->faker->paragraph(),
+            'due_date' => $this->faker->dateTimeBetween('now', '+2 months'),
+            'status' => 'new',
+        ];
+    }
+}
