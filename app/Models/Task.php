a1ef627134f68e54bd8ba0aa105a6db4c0e15476
donz <donnygapulao@gmail.com>
2025-11-18 22:24:29 +0800

    added has factory and fillables

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'title',
+        'user_id',
+        'description',
+        'due_date',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string,string>
+     */
+    protected $casts = [
+        'completed' => 'boolean',
+        'due_date' => 'datetime',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
