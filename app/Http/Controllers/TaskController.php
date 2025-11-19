7050889b5ead598d23c4f7f1c6d1dbede2d42a17
donz <donnygapulao@gmail.com>
2025-11-19 15:58:21 +0800

    move logic to service

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,44 +5,34 @@ namespace App\Http\Controllers;
 use App\Http\Requests\TaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
+use App\Services\TaskService;
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(TaskService $taskService)
     {
-        $tasks = Task::query()->with('user');
-        if (request()->has('due_date')) {
-            $tasks->whereDate('due_date', request()->input('due_date'));
-        }
-
-        return TaskResource::collection($tasks->paginate(10)->withQueryString());
+        return TaskResource::collection($taskService->paginatedTasks());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(TaskRequest $request)
+    public function store(TaskRequest $request, TaskService $taskService)
     {
-        Task::create($request->input());
-    }
+        $task = $taskService->createTask($request->input());
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Task $task)
-    {
-        //
+        return new TaskResource($task);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(TaskRequest $request, Task $task)
+    public function update(TaskRequest $request, Task $task, TaskService $taskService)
     {
-        $task->update($request->input());
+        $taskService->updateTask($task, $request->input());
     }
 
     /**
